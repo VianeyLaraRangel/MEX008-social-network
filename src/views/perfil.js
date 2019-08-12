ebeee54068760eca4a401f4a1775f931d85d108f
trinitysilvia <trinitysilviag@gmail.com>
2019-08-12 13:36:09 -0500

    agregando tablero de elementos guardados en perfil del usuario

--- src/views/perfil.js
+++ src/views/perfil.js
@@ -2,7 +2,7 @@ let perfil = {
     render: async () => {
         const view =
 
-        `<nav class="navbar navbar-expand-sm bg-danger navbar-dark text-white">
+        `<nav class="navbar navbar-expand-sm bg-warning navbar-dark text-white">
         <!-- Brand/logo -->
  <a class="navbar-brand">
    <img src="./img/avatar.jpg" alt="avatar" style="width:90px;" id="perfil-avatar">
@@ -11,7 +11,6 @@ let perfil = {
  <!-- Links --></a>
  <ul class="navbar-nav">
    <li class="nav-item">
-   <p> Hola, José </p>
    <li class="nav-item">
      <a  class="nav-link"><i class="far fa-comment-dots"></i> Mis publicaciones</a>
    </li>
@@ -32,10 +31,16 @@ let perfil = {
  <button id="btn-search"class=" btn btn-outline-success my-2 my-sm-0 btn btn-outline-warning p-2 mb-2 bg-white text-dark" type="submit"><i class="fas fa-search"></i></button>
    </form>
 </nav>
-        
-        <h1>AQUI VAMOS A VER EL PERFIL DEL USUARIO</h1>
-        <button class="btn btn-primary btn-usuario" id="usuario-button">ALERTA</button>`
 
+<section class="tablero">
+<button type="button" id="btn-historia" class="btn-lg btn-info"> #Historia </button>
+<button type="button" id="btn-cuentos"  class="btn-lg btn-success"> "Cuentos" </button>
+<button type="button" id="btn-videos"   class="btn-lg btn-danger"> Videos </button>
+<button type="button" id="btn-galeria"  class="btn-lg btn-purple"> Galeria </button>
+</section>
+`
+        
+        
         return view
     },
     after_render: async () => {
