668a5f6b23cf6b53feba001621f0dfe0452a8e32
trinitysilvia <trinitysilviag@gmail.com>
2019-08-12 10:06:56 -0500

    haciendo template para el inicio a la aplicación

--- src/views/inicio.js
+++ src/views/inicio.js
@@ -1,7 +1,15 @@
 let inicio = { 
     render : async () => {
-        let view = 
-    `<p>Inicio </p>`
+        const view = 
+    //`<p>Inicio </p>`
+    `<header class="cabecera-inicio">
+    <h1>Hola Usuario</h1>
+    </header>
+        <div class="registrar-box">
+             <h6>Aqui va el contenido de inicio</h6>
+        </div>`
+
+        return view;
     },
     after_render : async () => {
         alert('Continuamos');
